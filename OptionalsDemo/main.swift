bd965a946dcde6c5140379b8414ce215e7551bb3
Teodor <teodorttodorov@icloud.com>
2024-01-16 12:19:52 +0200

    Nil Coalescing Operator - Optional use

--- OptionalsDemo/main.swift
+++ OptionalsDemo/main.swift
@@ -1,12 +1,7 @@
 
 let myOptional: String?
 
-myOptional = "Teo"
+myOptional = nil
 
-if let safeOptional = myOptional {
-        let text: String = safeOptional
-        let text2: String = safeOptional
-        print(safeOptional)
-} else {
-    print("myOptional was found to be nil.")
-}
+let safeOptional = myOptional ?? "I am the default value."
+print(safeOptional)
